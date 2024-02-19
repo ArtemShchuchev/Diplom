c8e579a4df40eb033d76fb9e0a1ec2bf5904f52d
ArtemShchuchev <shuaa@rambler.ru>
2024-02-19 07:54:56 +0700

    исправлено декодирование

--- UrlEncodeDecode/UrlEncodeDecode.cpp
+++ UrlEncodeDecode/UrlEncodeDecode.cpp
@@ -2,22 +2,52 @@
 
 std::string url_decode(const std::string& encoded)
 {
-	std::string res;
-	std::istringstream iss(encoded);
-	char ch;
-
-	while (iss.get(ch)) {
-		if (ch == '%') {
-			int hex;
-			iss >> std::hex >> hex;
-			res += static_cast<char>(hex);
+	std::string output, hex_str;
+	unsigned count(0);
+	for (const auto& ch : encoded) {
+		if (ch == '%' || count) {
+			switch (count)
+			{
+				case 1:
+					hex_str += ch;
+					[[fallthrough]];
+				case 0:
+					++count;
+					break;
+				case 2:
+				{
+					hex_str += ch;
+					std::stringstream ss;
+					ss << std::hex << hex_str;
+					uint32_t uint(0);
+					ss >> uint;
+					output += static_cast<unsigned char>(uint);
+					count = 0;
+					hex_str.clear();
+					break;
+				}
+				default:
+					break;
+			}
 		}
-		else {
-			res += ch;
-		}
+		else output += ch;
 	}
+	/*
+	std::string output;
+	for (size_t i(0); i < encoded.length(); ++i) {
+		if (encoded[i] == '%') {
+			std::stringstream ss;
+			ss << std::hex << encoded.substr(++i, 2);
+			++i;
+			uint32_t uint(0);
+			ss >> uint;
+			output.push_back(static_cast<unsigned char>(uint));
+		}
+		else output.push_back(encoded[i]);
+	}
+	*/
 
-	return res;
+	return output;
 }
 
 std::string url_encode(const std::string& decoded)
