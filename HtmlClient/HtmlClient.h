af25da9abeabf0fd6e8acfe063ca40aefb612b21
ArtemShchuchev <shuaa@rambler.ru>
2024-02-21 21:32:29 +0700

    delete some heders

--- HtmlClient/HtmlClient.h
+++ HtmlClient/HtmlClient.h
@@ -4,10 +4,8 @@
 #include <boost/beast.hpp>
 #include <boost/url.hpp>
 #include <boost/asio.hpp>
-#include <boost/regex.hpp>
 #include <boost/asio/ssl.hpp>
 #include "SecondaryFunction.h"
-#include "UrlEncodeDecode.h"
 
 namespace beast = boost::beast;     // from <boost/beast.hpp>
 namespace http = beast::http;       // from <boost/beast/http.hpp>
